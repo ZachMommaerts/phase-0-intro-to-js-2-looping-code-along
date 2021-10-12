3c9c043ddf672375b5711c0425e777d2b00cd6cc
Zach Mommaerts <zmommaerts@gmail.com>
2021-10-11 19:00:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function writeCards (recipients, events) {
+
+    const messages = [];
+
+    for (let i = 0; i < recipients.length; i++) {
+        const thankYouMessage = `Thank you, ${recipients[i]}, for the wonderful ${events} gift!`;
+        messages.push(thankYouMessage);
+    }
+
+    return messages;
+}
+
+function countDown (integer) {
+    while (integer != -1) {
+        console.log(integer);
+        integer--;
+    }
+}
